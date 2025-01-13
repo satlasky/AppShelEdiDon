bfa9400134123b559c0a34583a2009a94099a315
satlasky <sam.atlaski@gmail.com>
2025-01-13 10:29:28 +0300

    last commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppShelEdiDon
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
